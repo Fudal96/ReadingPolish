fa234b0d3b6f40f37ecfa30968318d58ac04ad56
Fudal96 <fudal22@gmail.com>
2023-04-18 00:21:07 +0200

    Quiz started

--- src/app/a1stories/a1-co-lubie-robic/a1-co-lubie-robic.component.ts
+++ src/app/a1stories/a1-co-lubie-robic/a1-co-lubie-robic.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class A1CoLubieRobicComponent {
 
+points = 0;
+
+
+
+
+
+
+  checkAnswer(event: any) {
+    const answer = event.target.dataset.answer;
+    const element = event.target
+    const parent = event.target.parentElement;
+    if (answer === 'correct') {
+      element.classList.add('correct')
+     parent.classList.add('disable')
+     this.points++;
+     console.log(this.points)
+    } else if (answer === 'false') {
+      element.classList.add('false')
+      parent.classList.add('disable')
+    } else {
+      console.log('error')
+    }
+  }
+
+
+
 }
+
+
