22656e3b7ab188a3b18f45d273f913a45d730410
Fudal96 <fudal22@gmail.com>
2023-04-21 00:06:50 +0200

    Quiz is working

--- src/app/a1stories/a1-co-lubie-robic/a1-co-lubie-robic.component.ts
+++ src/app/a1stories/a1-co-lubie-robic/a1-co-lubie-robic.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-a1-co-lubie-robic',
   templateUrl: './a1-co-lubie-robic.component.html',
   styleUrls: ['./a1-co-lubie-robic.component.scss']
 })
-export class A1CoLubieRobicComponent {
 
+
+
+export class A1CoLubieRobicComponent {
 points = 0;
+answered = 0;
+//@ViewChild('reset5') resetAnswers: ElementRef | undefined;
+@ViewChildren('reset, reset2, reset3, reset4, reset5', {read: ElementRef}) childComp:QueryList<ElementRef> | undefined
 
 
 
@@ -22,13 +27,28 @@ points = 0;
       element.classList.add('correct')
      parent.classList.add('disable')
      this.points++;
-     console.log(this.points)
+     this.answered++;
     } else if (answer === 'false') {
       element.classList.add('false')
       parent.classList.add('disable')
+      this.answered++;
     } else {
       console.log('error')
     }
+  }
+
+  restartTest() {
+    this.childComp?.forEach(x => {
+      x.nativeElement.classList.remove('disable')
+      const children = x.nativeElement.children
+      const getEachChild = [...children]
+      getEachChild.forEach(btn => {
+        btn.classList.remove('correct')
+        btn.classList.remove('false')
+      })
+    })
+    this.points = 0;
+    this.answered = 0;
   }
 
 
